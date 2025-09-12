c2bac94603f6f30fdb361a36e6c52dd963d1d722
Ryan Lucas <ryanlcampos19@gmail.com>
2025-09-12 14:53:04 -0300

    :wrench: Chore: ajustar serviço para setar vendedor ao salvar venda

--- src/main/java/com/ryanlcampos/xbrain/domain/service/VendaService.java
+++ src/main/java/com/ryanlcampos/xbrain/domain/service/VendaService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ryanlcampos.xbrain.domain.model.Venda;
+import com.ryanlcampos.xbrain.domain.model.Vendedor;
 import com.ryanlcampos.xbrain.domain.repository.VendaRepository;
 @Service
 public class VendaService {
@@ -11,7 +12,16 @@ public class VendaService {
     @Autowired
     private VendaRepository vendaRepository;
 
+    @Autowired
+    private VendedorService vendedorService;
+
     public Venda salvar(Venda venda) {
+
+        
+        Vendedor vendedor = vendedorService.obterPorId(venda.getVendedor().getId());
+
+        venda.setVendedor(vendedor);
+        
         return vendaRepository.save(venda);
     }
 }
