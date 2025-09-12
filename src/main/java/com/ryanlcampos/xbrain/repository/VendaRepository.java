371a21709b788f7d0b0ef6ba176d8d73ce460590
Ryan Lucas <ryanlcampos19@gmail.com>
2025-09-12 13:42:19 -0300

    :sparkles: Feat: implementa repository para Venda

---
+++ src/main/java/com/ryanlcampos/xbrain/repository/VendaRepository.java
@@ -0,0 +1,9 @@
+package com.ryanlcampos.xbrain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ryanlcampos.xbrain.domain.Venda;
+
+public interface VendaRepository extends JpaRepository<Venda, Long>{
+
+}
