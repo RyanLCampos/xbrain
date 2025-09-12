9ca024e0c54a62f234a1baf8c881389d739a53be
Ryan Lucas <ryanlcampos19@gmail.com>
2025-09-12 13:33:58 -0300

    :sparkles: Feat: implementa entity Venda

---
+++ src/main/java/com/ryanlcampos/xbrain/domain/Venda.java
@@ -0,0 +1,31 @@
+package com.ryanlcampos.xbrain.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded=true)
+@Data
+@Entity
+@Table
+public class Venda {
+
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime dataVenda;
+
+    @Column(nullable = false)
+    private BigDecimal valor;
+}
