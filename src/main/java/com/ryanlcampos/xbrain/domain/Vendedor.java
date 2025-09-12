0a24a51885aac3535c1b58b3b70d8b7874d9c184
Ryan Lucas <ryanlcampos19@gmail.com>
2025-09-12 13:37:45 -0300

    :sparkles: Feat: implementa entity Vendedor

---
+++ src/main/java/com/ryanlcampos/xbrain/domain/Vendedor.java
@@ -0,0 +1,25 @@
+package com.ryanlcampos.xbrain.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+@Entity
+@Table
+public class Vendedor {
+
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+}
